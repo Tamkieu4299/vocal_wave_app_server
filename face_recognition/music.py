a7112fd50c188689e1042e9d8bd846ed0266ec03
Thanh Nguyen <95325717+bkdnltc2002@users.noreply.github.com>
2023-12-14 13:08:28 +0700

    undo all works

--- face_recognition/music.py
+++ face_recognition/music.py
@@ -1,12 +1,9 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
 import av
 import cv2 
 import numpy as np 
 import mediapipe as mp 
 from keras.models import load_model
-import webbrowser
-
 
 model  = load_model("model.h5")
 label = np.load("labels.npy")
@@ -15,54 +12,50 @@ hands = mp.solutions.hands
 holis = holistic.Holistic()
 drawing = mp.solutions.drawing_utils
 
-st.header("Emotion Based Music Recommender")
 
-class EmotionProcessor:
-	def recording(self,frame):
-		data_size = 0
-		while True:
-			frm = frame.to_ndarray(format="bgr24")
+def recording():
+	cap =cv2.VideoCapture(0)
+	data_size = 0
+	while True:
+
+		_, frm = cap.read()
+
+		frm = cv2.flip(frm, 1)
+
+		res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+
+		lst = []
 			
-			frm = cv2.flip(frm, 1)
+		if res.face_landmarks:
+			for i in res.face_landmarks.landmark:
+				lst.append(i.x - res.face_landmarks.landmark[1].x)
+				lst.append(i.y - res.face_landmarks.landmark[1].y)
 
-			res = holis.process(cv2.cvtColor(frm, cv2.COLOR_BGR2RGB))
+			data_size = data_size+1
+    
+		lst = np.array(lst).reshape(1,-1)
 
-			lst = []
+		pred = label[np.argmax(model.predict(lst))]
+
+		print(pred)
+		cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
+
 				
-			if res.face_landmarks:
-				for i in res.face_landmarks.landmark:
-					lst.append(i.x - res.face_landmarks.landmark[1].x)
-					lst.append(i.y - res.face_landmarks.landmark[1].y)
-
-				data_size = data_size+1
-		
-			lst = np.array(lst).reshape(1,-1)
-
-			pred = label[np.argmax(model.predict(lst))]
-
-			print(pred)
-			cv2.putText(frm, pred, (50,50),cv2.FONT_ITALIC, 1, (255,0,0),2)
-
-					
-			drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
-										landmark_drawing_spec=drawing.DrawingSpec(color=(0,0,255), thickness=-1, circle_radius=1),
-										connection_drawing_spec=drawing.DrawingSpec(thickness=1))
+		drawing.draw_landmarks(frm, res.face_landmarks, holistic.FACEMESH_TESSELATION,
+									landmark_drawing_spec=drawing.DrawingSpec(color=(0,0,255), thickness=-1, circle_radius=1),
+									connection_drawing_spec=drawing.DrawingSpec(thickness=1))
 
 
-			cv2.imshow("window",frm)
+		cv2.imshow("window",frm)
 
-			if cv2.waitKey(1) == 27 or data_size >39:
-				with open('emotion.txt','w') as f:
-					f.write(str(pred))
-				cv2.destroyAllWindows()
-				return 	av.VideoFrame.from_ndarray(frm, format="bgr24")
-				break
-		return 	av.VideoFrame.from_ndarray(frm, format="bgr24")
+		if cv2.waitKey(1) == 27 or data_size >39:
+			with open('emotion.txt','w') as f:
+				f.write(str(pred))
+			cv2.destroyAllWindows()
+			cap.release()
+			break
 
-
-
-webrtc_streamer(key="key", desired_playing_state=True,
-				video_processor_factory=EmotionProcessor)
+recording()
 
 # if btn:
 # 	if not(emotion):
